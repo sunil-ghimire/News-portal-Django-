bcaf2f482bc55e7bacebd46974a6dade104f17ba
pox420 <pox420@gmail.com>
2021-12-12 11:34:34 +0545

    allowed all server

--- STM/settings.py
+++ STM/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'qha4cc*uf^&vzdrfy#+)6xvs)b&k&2nwrq80o1rz#h#_07u16t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
